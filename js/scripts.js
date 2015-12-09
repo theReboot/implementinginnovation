e1013988066e5815616e7d7fc570d989110a8ded
Adam Parker <adam@theadamparker.com>
2015-12-09 12:33:33 -0500

    Fixed the accordion to allow toggling, and closing only one accordion at a time.

--- js/scripts.js
+++ js/scripts.js
@@ -13,19 +13,19 @@ if (!Modernizr.svg) {
 //accordion
 
 jQuery(document).ready(function() {
-	function close_accordion_section() {
-		jQuery('.accordion .accordion-section-title').removeClass('active');
-		jQuery('.accordion .accordion-section-content').slideUp(300).removeClass('open');
-	}
 
 	jQuery('.accordion-section-title').click(function(e) {
 		// Grab current anchor value
 		var currentAttrValue = jQuery(this).attr('href');
 
-		if(jQuery(e.target).is('.active')) {
-			close_accordion_section();
+		if(jQuery(this).is('.active')) {
+			// close_accordion_section();
+			// jQuery('.accordion .accordion-section-title').removeClass('active');
+			// jQuery('.accordion .accordion-section-content').slideUp(300).removeClass('open');
+			jQuery(this).removeClass('active');
+			jQuery(currentAttrValue).slideUp(300).removeClass('open');
 		}else {
-			close_accordion_section();
+			// close_accordion_section();
 
 			// Add active class to section title
 			jQuery(this).addClass('active');
